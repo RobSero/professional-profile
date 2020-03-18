4b92e6ca1491cdac96d84628024707f0b0f2eb33
Rob Serowka <robserowka@Robs-MacBook-Pro.local>
2020-03-18 16:05:47 +0000

    first upload to heroku

---
+++ app.js
@@ -0,0 +1,29 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+
+let app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname + '/public'));
+
+app.listen(3000, function(){
+    console.log('Website up and running!')
+})
+
+// --------------------------- GET/POST SECTION --------------------------------
+
+app.get('/', function(req,res){
+    res.sendFile(__dirname + '/home.htm');
+}) 
+
+app.get('/webDev', function(req,res){
+    res.sendFile(__dirname + '/web_dev.htm');
+}) 
+
+app.get('/architecture', function(req,res){
+    res.sendFile(__dirname + '/architecture.htm');
+}) 
+
+app.get('/graphics', function(req,res){
+    res.sendFile(__dirname + '/graphic_design.htm');
+});
