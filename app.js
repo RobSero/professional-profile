44225716e90ffa72f574108550807ab34af25e87
Rob Serowka <robert.serowka@hotmail.co.uk>
2020-06-30 16:09:22 +0100

    Test

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-let express = require('express');
-let bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
 
-let app = express();
+const app = express()
 
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/public'))
 
 app.listen(process.env.PORT  || 3000, function(){
-    console.log('Website up and running!')
+  console.log('Website up and running!')
 })
 
 // --------------------------- GET/POST SECTION --------------------------------
 
 app.get('/', function(req,res){
-    res.sendFile(__dirname + '/home.htm');
+  res.sendFile(__dirname + '/home.htm')
 }) 
 
 app.get('/webDev', function(req,res){
-    res.sendFile(__dirname + '/web_dev.htm');
+  res.sendFile(__dirname + '/web_dev.htm')
 }) 
 
 app.get('/architecture', function(req,res){
-    res.sendFile(__dirname + '/architecture.htm');
+  res.sendFile(__dirname + '/architecture.htm')
 }) 
 
 app.get('/graphics', function(req,res){
-    res.sendFile(__dirname + '/graphic_design.htm');
-});
+  res.sendFile(__dirname + '/graphic_design.htm')
+})
--- public/js/index.js
+++ public/js/index.js
@@ -1,72 +1,72 @@
 
 $(function() {
-let col1 = document.getElementById('column1');
-let col2 = document.getElementById('column2');
-let col3 = document.getElementById('column3');
-let col4 = document.getElementById('column4');
-let columnFormatting = document.getElementsByClassName('columns');
-let backButton = document.getElementById('back-button');
+  const col1 = document.getElementById('column1')
+  const col2 = document.getElementById('column2')
+  const col3 = document.getElementById('column3')
+  const col4 = document.getElementById('column4')
+  const columnFormatting = document.getElementsByClassName('columns')
+  const backButton = document.getElementById('back-button')
 
-col1.addEventListener('mouseover', function () {
-    col1.style.width = '30%';
-    col2.style.width = '18%';
-    col3.style.width = '24%';
-    col4.style.width = '24%';
-})
-col2.addEventListener('mouseover', function () {
-    col1.style.width = '21%';
-    col2.style.width = '30%';
-    col3.style.width = '21%';
-    col4.style.width = '24%';
-})
-col3.addEventListener('mouseover', function () {
-    col1.style.width = '24%';
-    col2.style.width = '21%';
-    col3.style.width = '30%';
-    col4.style.width = '21%';
-})
-col4.addEventListener('mouseover', function () {
-    col1.style.width = '24%';
-    col2.style.width = '24%';
-    col3.style.width = '18%';
-    col4.style.width = '30%';
-})
+  col1.addEventListener('mouseover', function () {
+    col1.style.width = '30%'
+    col2.style.width = '18%'
+    col3.style.width = '24%'
+    col4.style.width = '24%'
+  })
+  col2.addEventListener('mouseover', function () {
+    col1.style.width = '21%'
+    col2.style.width = '30%'
+    col3.style.width = '21%'
+    col4.style.width = '24%'
+  })
+  col3.addEventListener('mouseover', function () {
+    col1.style.width = '24%'
+    col2.style.width = '21%'
+    col3.style.width = '30%'
+    col4.style.width = '21%'
+  })
+  col4.addEventListener('mouseover', function () {
+    col1.style.width = '24%'
+    col2.style.width = '24%'
+    col3.style.width = '18%'
+    col4.style.width = '30%'
+  })
 
-for (let i = 0; i < columnFormatting.length; i++) {
-    columnFormatting[i].height = window.innerHeight;
+  for (let i = 0; i < columnFormatting.length; i++) {
+    columnFormatting[i].height = window.innerHeight
     columnFormatting[i].addEventListener('mouseleave', function () {
-        this.style.filter = 'grayscale(100%)';
-        this.getElementsByClassName('inner-text')[0].style.visibility = 'hidden';
-        this.getElementsByTagName('h3')[0].removeAttribute('class');
-        col1.style.width = '24%';
-        col2.style.width = '24%';
-        col3.style.width = '24%';
-        col4.style.width = '24%';
-    });
+      this.style.filter = 'grayscale(100%)'
+      this.getElementsByClassName('inner-text')[0].style.visibility = 'hidden'
+      this.getElementsByTagName('h3')[0].removeAttribute('class')
+      col1.style.width = '24%'
+      col2.style.width = '24%'
+      col3.style.width = '24%'
+      col4.style.width = '24%'
+    })
 
     columnFormatting[i].addEventListener('mouseover', function () {
-        this.style.filter = 'grayscale(50%)';
-        this.getElementsByClassName('inner-text')[0].style.visibility = 'visible';
-        this.getElementsByTagName('h3')[0].setAttribute('class', 'highlighted');
+      this.style.filter = 'grayscale(50%)'
+      this.getElementsByClassName('inner-text')[0].style.visibility = 'visible'
+      this.getElementsByTagName('h3')[0].setAttribute('class', 'highlighted')
 
     })
-}
+  }
 
-col1.addEventListener('click', function(){
-    $('.pop-up').slideDown('slow');
-})
+  col1.addEventListener('click', function(){
+    $('.pop-up').slideDown('slow')
+  })
 
-backButton.addEventListener('click', function(){
-    $('.pop-up').slideUp('slow');
-})
+  backButton.addEventListener('click', function(){
+    $('.pop-up').slideUp('slow')
+  })
 
-$('.drop-down-btn').click(function(){
-    $('.dropdown-social').slideDown('fast');
-    $('.faded-view').show();
-})
-$('.faded-view').click(function(){
-    $('.dropdown-social').slideUp('fast');
-    $('.faded-view').hide();
-})
+  $('.drop-down-btn').click(function(){
+    $('.dropdown-social').slideDown('fast')
+    $('.faded-view').show()
+  })
+  $('.faded-view').click(function(){
+    $('.dropdown-social').slideUp('fast')
+    $('.faded-view').hide()
+  })
 })
 
